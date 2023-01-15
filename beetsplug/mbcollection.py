d98659300ed547f94357c326c4e7a9b2117eabc0
Louis Rannou <louis.rannou@gresille.org>
2023-01-15 23:57:42 +0100

    oauth2

--- beetsplug/mbcollection.py
+++ beetsplug/mbcollection.py
@@ -54,11 +54,24 @@ def submit_albums(collection_id, release_ids):
 class MusicBrainzCollectionPlugin(BeetsPlugin):
     def __init__(self):
         super().__init__()
+        self.oauth = None
         config['musicbrainz']['pass'].redact = True
-        musicbrainzngs.auth(
-            config['musicbrainz']['user'].as_str(),
-            config['musicbrainz']['pass'].as_str(),
-        )
+        config['musicbrainz']['client_secret'].redact = True
+        config['musicbrainz']['access_token'].redact = True
+        config['musicbrainz']['refresh_token'].redact = True
+        if 'user' in config['musicbrainz']:
+            musicbrainzngs.auth(
+                config['musicbrainz']['user'].as_str(),
+                config['musicbrainz']['pass'].as_str(),
+            )
+        elif 'cliend_id' in config['musicbrainz']:
+            musicbrainzngs.oauth(
+                config['musicbrainz']['client_id'].as_str(),
+                config['musicbrainz']['client_secret'].as_str(),
+                )
+            self.oauth = musicbrainzngs.OAuth(scope=['collection'])
+        else:
+            raise ui.UserError('No authentication credential in the configuration')
         self.config.add({
             'auto': False,
             'collection': '',
@@ -66,6 +79,81 @@ class MusicBrainzCollectionPlugin(BeetsPlugin):
         })
         if self.config['auto']:
             self.import_stages = [self.imported]
+
+    def oauth_setup(self):
+        c_key = self.config['musicbrainz']['client_id'].as_str()
+        c_secret = self.config['musicbrainz']['client_secret'].as_str()
+
+        # Get the OAuth token from a file or log in.
+        if 'access_token' in self.config['musicbrainz']:
+            access_token = self.config['musicbrainz']['access_token']
+            self.set_token(access_token)
+        elif 'refresh_token' in self.config['musicbrainz']:
+            refresh_token = self.config['musicbrainz']['access_token']
+            refresh_token()
+        else:
+            raise ui.UserError('No authentication credential. Run command mboauth first.')
+
+    def authenticate(self, c_key, c_secret):
+        # Get the link for the OAuth page.
+        url = self.oauth.get_authorization_url(code)
+        if not url:
+            self._log.debug('oauth error: could not create authorize url')
+            raise beets.ui.UserError('communication with MusicBrainz failed')
+
+        beets.ui.print_("To authenticate with Beatport, visit:")
+        beets.ui.print_(url)
+
+        # Ask for the verifier data and validate it.
+        data = beets.ui.input_("Enter the string displayed in your browser:")
+        authorization = self.oauth.get_authorization(data)
+        if not authorization:
+            self._log.debug('oauth error: could not exchange code')
+            raise beets.ui.UserError('MusicBrainz token request failed')
+
+        # Save the token for later use.
+        token, secret = (authorization['access_token'], authorization['refresh_token'])
+        self._log.debug('MusicBrainz token {0}, secret {1}', token, secret)
+        self.config.add({
+            'access_token': token
+            'refresh_token': secret
+            })
+
+        return token, secret
+
+    def refresh_token(self):
+        """Refresh the access token
+        """
+        if not 'refresh_token' in config:
+            self._log.debug('No refresh token in the configuration')
+            raise beets.ui.UserError('authenticate first')
+        authorization = self.oauth.refresh_token(self.config['refresh_token'].as_str())
+        if not authorization:
+            self._log.debug('oauth error: could not exchange code')
+            raise beets.ui.UserError('MusicBrainz token request failed')
+
+        # Save the token for later use.
+        token = authorization['access_token']
+        self._log.debug('MusicBrainz token {0}', token)
+        self.config.add({
+            'access_token': token
+            })
+
+        return access_token
+
+    def revoke_token(self, access=True, refresh=False):
+        """Revoke the access token or the refresh token
+
+        :param access:   if unset, do not revoke the access token
+        :param refresh:  if set, revoke the refresh token
+        """
+        if not 'access_token' in config:
+            self._log.debug('OAuth2 has not been initialized')
+            raise beets.ui.UserError('authenticate first')
+        if access:
+            self.oauth.revoke_token(self.config['access_token'].as_str())
+        if refresh:
+            self.oauth.revoke_token(self.config['refresh_token'].as_str())
 
     def _get_collection(self):
         collections = mb_call(musicbrainzngs.get_collections)
@@ -115,7 +203,20 @@ class MusicBrainzCollectionPlugin(BeetsPlugin):
                                    dest='remove',
                                    help='Remove albums not in beets library')
         mbupdate.func = self.update_collection
-        return [mbupdate]
+        mboauth = SubCommand('auth',
+                             help='Manage the OAuth2 authentication')
+        mboauth.parser.add_option('-r', '--revoke',
+                                  default=None,
+                                  dest='revoke',
+                                  help='Revoke authentication')
+        mboauth.func = self.mboauth
+        return [mbupdate, mboauth]
+
+    def mboauth(self):
+        if self.config['revoke'].get(bool):
+            self.revoke(refresh=True)
+        else
+            self.authenticate()
 
     def remove_missing(self, collection_id, lib_albums):
         lib_ids = {x.mb_albumid for x in lib_albums}
@@ -129,6 +230,8 @@ class MusicBrainzCollectionPlugin(BeetsPlugin):
             )
 
     def update_collection(self, lib, opts, args):
+        if 'access_token' in self.config['musicbrainz']:
+            self.oauth_setup()
         self.config.set_args(opts)
         remove_missing = self.config['remove'].get(bool)
         self.update_album_list(lib, lib.albums(), remove_missing)
